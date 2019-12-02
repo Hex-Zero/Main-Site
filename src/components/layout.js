9a277a43b3581bdefe9181eb59496084f0934f97
Hex-Zero <hex0@live.com>
2019-12-02 17:21:03 +0000

    Setup Complete

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import "../styles/index.scss"
+import "../style/index.scss"
 
 import Header from "./header"
 
@@ -18,14 +18,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div>
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <main>{children}</main>
     </>
   )
 }
