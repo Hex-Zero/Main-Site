3667edba49bcfcea65a693521d872404135de4b0
Hex-Zero <hex0@live.com>
2019-12-03 19:22:36 +0000

    name is now a link

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,12 +6,22 @@ import AniLink from "gatsby-plugin-transition-link/AniLink"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div className="index-name">
-      <h1 className="text-spark">Hex-Zero</h1>
-      <h2 className="name-aka">
-        <span className="aka">aka:</span> Mantas Brasiunas
-      </h2>
-    </div>
+    <AniLink
+      duration={1}
+      activeClassName="active"
+      paintDrip
+      hex="#852766"
+      to="contact"
+      className=""
+    >
+      <div className="index-name">
+        <h1 className="text-spark">Hex-Zero</h1>
+        <h2 className="name-aka">
+          <span className="aka">aka:</span> Mantas Brasiunas
+        </h2>
+      </div>
+    </AniLink>
+
     <AniLink
       duration={1}
       activeClassName="active"
