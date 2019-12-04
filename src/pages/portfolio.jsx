95a6b1618c59a9eaad02f41b775201f57b105d33
Hex-Zero <hex0@live.com>
2019-12-04 14:59:03 +0000

    portfolio links

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -3,10 +3,31 @@ import Layout from "../components/layout"
 import ImageLink400 from "../images/imageLink"
 
 const portfolio = () => {
+  const info = [
+    { imageName: "mi" },
+    { imageName: "mi" },
+    { imageName: "mi" },
+    { imageName: "mi" },
+  ]
   return (
     <Layout>
       <div className="portfolio">
-        <ImageLink400 imageName="mi" className="image-link400" />
+        {info.map(item => {
+          return (
+            <div className="link-container">
+              <div className="links">
+                <ImageLink400
+                  imageName={item.imageName}
+                  className="image-link400"
+                />
+                <h1 className="link-text">
+                  lotnsraeitnsreiatn ser natei asrnetoi nsraei thrsei hteisr
+                  nteisr niers
+                </h1>
+              </div>
+            </div>
+          )
+        })}
       </div>
     </Layout>
   )
