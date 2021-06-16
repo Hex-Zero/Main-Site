152032480ed9272eb2760f23e1606cb93a374449
Ace Alrich <hex0@live.com>
2021-06-17 00:15:59 +0100

    fix shitty stuff

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,18 +2,13 @@ import React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/SEO"
 import ContactForm from "../components/contactForm"
-import SocialLink from "../components/socialLinks"
 const Contact = () => {
   return (
     <Layout>
       <Seo title="Contact" />
 
       <div className="contact">
-        <div></div>
         <ContactForm />
-        <div className="links">
-          <SocialLink />
-        </div>
       </div>
     </Layout>
   )
