5e94025973cd0766db0308ade26470417d4994d7
Ace Alrich <hex0@live.com>
2021-07-19 21:29:53 +0100

    who need a title

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -16,7 +16,6 @@ function SEO({ description, lang, meta, title }) {
       query {
         site {
           siteMetadata {
-            title
             description
             author
           }
